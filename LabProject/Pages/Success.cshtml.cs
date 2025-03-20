5802af05ca107bbd79421ea548cf294551df574e
Barkin06 <akkayabarkin@hotmail.com>
2025-03-20 14:34:25 +0300

    week4 lab

---
+++ LabProject/Pages/Success.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LabProject.Pages
+{
+    public class SuccessModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
